5d53570da771d018f848c68302c6638199ce650c
Mao <929764654@qq.com>
2020-01-30 12:39:39 +0800

    外观模式
    向现有的系统添加一个接口（类），来隐藏系统的复杂性
    2020-01-30

---
+++ Assets/02Scripts/GameFacade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 外观模式（Facade Pattern）
+/// 隐藏系统的复杂性，并向客户端提供了一个客户端可以访问系统的接口。
+/// 这种类型的设计模式属于结构型模式，它向现有的系统添加一个接口，来隐藏系统的复杂性。
+/// </summary>
+public class GameFacade
+{
+    private bool m_IsGameOver = false;
+    public bool isGameOver { get { return m_IsGameOver; } }
+
+    //使用单例模式
+    private static GameFacade m_Instance;
+    public static GameFacade GetInstance()
+    {
+        if (m_Instance == null)
+            m_Instance = new GameFacade();
+        return m_Instance;
+    }
+
+    private GameFacade() { }
+
+
+    public void Init()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+    public void Release()
+    {
+
+    }
+}
--- Assets/02Scripts/SceneState/BattleState.cs
+++ Assets/02Scripts/SceneState/BattleState.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-
+/// <summary>
+/// 战斗状态
+/// </summary>
 class BattleState : ISceneState
 {
+    //private GameFacade m_Facade; //外观模式的引用
+
     public BattleState(  SceneStateControl controller) : base("03BattleState", controller)
     { 
     }
+
+    public override void StateStart()
+    {
+        GameFacade.GetInstance().Init();
+    }
+
+    public override void StateUpdate()
+    {
+        GameFacade.GetInstance().Update();
+    }
+    public override void StateEnd()
+    {
+        if (GameFacade.GetInstance().isGameOver)
+        {
+            m_controller.SetState(new MainMenuState(m_controller));
+        }
+        GameFacade.GetInstance().Release();
+    }
+
 }
 
